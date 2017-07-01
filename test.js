9622a90ea3e5e50c3d7529cccb4cd2078a5b533e
Hugo <h@hugo.sx>
2017-07-01 12:33:40 +1000

    ✅ Add tests

---
+++ test.js
@@ -0,0 +1,23 @@
+const test = require('ava');
+
+const isCurrencyCode = require('./index');
+
+test('should succeed with uppercase currency code', t => {
+  t.is(isCurrencyCode('usd'), true);
+});
+
+test('should succeed with lowercase currency code', t => {
+  t.is(isCurrencyCode('aud'), true);
+});
+
+test('should fail with invalid currency code', t => {
+  t.is(isCurrencyCode('not-a-currency-code'), false);
+});
+
+test('should fail with boolean', t => {
+  t.is(isCurrencyCode(true), false);
+});
+
+test('should fail with object', t => {
+  t.is(isCurrencyCode({}), false);
+});
